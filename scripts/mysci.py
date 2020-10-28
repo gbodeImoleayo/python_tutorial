29b4d5fdd0dd28b1c0f01078612d157ac54a9a9d
gbode <gbode@12049-mbp.fritz.box>
2020-10-28 21:11:59 +0100

    Initializing the dictionary and selecting with get

--- scripts/mysci.py
+++ scripts/mysci.py
@@ -1,5 +1,38 @@
 #--python is very sensitive to whitespace
 #--therefore it is important to be consistent with spaces
+
+#--Column names and column indices to read
+columns = {'date':0, 'time':1, 'tempout':2}
+
+#--Data types for each column  (only if non-string)
+types = {'tempout': float}
+
+#--initialize my data variable
+data = {}#{'date':[], 'time':[], 'tempout':[]} # --as dictionary
+
+for column in columns:
+    data[column] = []
+
+#--Read the data file
+filename='../data/wxobs20170821.txt'
+
+with open(filename, 'r') as datafile:
+    
+    #-- read the first three lines (header)
+    for _ in range(3):
+        datafile.readline()
+
+    #-- Read and parse the rest of the file
+    for line in datafile:
+        split_line = line.split()
+        for column in columns:
+            i = columns[column]
+            t = types.get(column, str)
+            value = t(split_line[i])
+            data[column].append(value)
+
+exit()
+
 #--initialize my data variable
 #data = [] # --as list
 data = {'date':[], 'time':[], 'tempout':[]} # --as dictionary
